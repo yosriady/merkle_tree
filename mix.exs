b72bb43a023f7a8039f123fefc878b8af0aa04d9
Yos Riady <yosriady@gmail.com>
2018-02-19 20:01:33 +0800

    1.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MerkleTree.Mixfile do
 
   def project do
     [app: :merkle_tree,
-     version: "1.2.1",
+     version: "1.3.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
