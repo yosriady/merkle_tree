5c04220ef83b4df6ef7d16b5327fdb3ed391d47b
Yos Riady <yosriady@gmail.com>
2020-01-30 15:02:02 +0800

    2.0.0 Release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MerkleTree.Mixfile do
   def project do
     [
       app: :merkle_tree,
-      version: "1.6.1",
+      version: "2.0.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -52,8 +52,8 @@ defmodule MerkleTree.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps() do
     [
-      {:ex_doc, "~> 0.11", only: :dev},
-      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
+      {:earmark, "~> 1.1", only: :dev},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.5", only: :test}
     ]
